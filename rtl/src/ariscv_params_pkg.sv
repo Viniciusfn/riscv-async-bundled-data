f64ab4090a30a5b5bcdbe44f66bb1ae1dccbead7
Vinicius de Franca <vinicius.franca@hwit.com.br>
2026-06-10 21:05:47 -0300

    [LC] Tokens_2 version on going (test fail)

--- rtl/src/ariscv_params_pkg.sv
+++ rtl/src/ariscv_params_pkg.sv
@@ -67,14 +67,25 @@ package ariscv_params_pkg;
 
          // Initialization
       `ifdef PROTO_LC
-      INIT_PC        : 1,
-      INIT_FD        : 0,
-      INIT_DE        : 0,
-      INIT_EM        : 0,
-      INIT_MW        : 0,
-      INIT_REG       : 1,
-      INIT_LOOP1     : 0,
-      INIT_LOOP2     : 0,
+         `ifdef TOKENS_1
+         INIT_PC        : 1,
+         INIT_FD        : 0,
+         INIT_DE        : 0,
+         INIT_EM        : 0,
+         INIT_MW        : 0,
+         INIT_REG       : 1,
+         INIT_LOOP1     : 0,
+         INIT_LOOP2     : 0,
+         `elsif TOKENS_2
+         INIT_PC        : 1,
+         INIT_FD        : 0,
+         INIT_DE        : 1,
+         INIT_EM        : 0,
+         INIT_MW        : 1,
+         INIT_REG       : 0,
+         INIT_LOOP1     : 0,
+         INIT_LOOP2     : 0,
+         `endif
       `else
       INIT_PC        : 1,
       INIT_FD        : 0,
